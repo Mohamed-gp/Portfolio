cb91d83da29208cfd3398092fb4762e84b4acfd0
Mohamed Outerbah <mohamedterba6@gmail.com>
2026-07-17 11:19:19 +0100

    remove portfolio-v2 experiment

--- portfolio-v2/src/components/3d/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all 3D components
-export { Scene } from "./Scene";
-export { Aircraft } from "./Aircraft";
-export { FloatingIsland } from "./FloatingIsland";
-export { Environment } from "./Environment";
-export { FlightController } from "./FlightController";
--- portfolio-v2/src/components/ui/index.ts
+++
@@ -1,6 +0,0 @@
-// Export all UI components
-export { LoadingScreen } from "./LoadingScreen";
-export { MissionBriefing } from "./MissionBriefing";
-export { HUD } from "./HUD";
-export { AircraftSelector } from "./AircraftSelector";
-export { IslandContent } from "./IslandContent";
